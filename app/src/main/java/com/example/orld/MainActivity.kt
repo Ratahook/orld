4c9aa3da37974ec0a20c8a141f1718010b2f8db0
Ratahook <leonidratahook@gmail.com>
2022-08-05 19:19:32 +0300

    Testing kotlin

--- app/src/main/java/com/example/orld/MainActivity.kt
+++ app/src/main/java/com/example/orld/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.orld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<TextView>(R.id.textView)
+        view.text = "Sorry for what?"
+        val viet = findViewById<TextView>(R.id.nexttext)
+        viet.text = "Oh, shit, im sorry"
+        val student = Student("Ivan Ivanov", 3)
+        val anotherStudent = Student("Dmitriy", 4)
+        val viewstudent = findViewById<TextView>(R.id.student)
+        viewstudent.text = student.toString()
+        val vietstudent = findViewById<TextView>(R.id.anotherStudent)
+        vietstudent.text = anotherStudent.toString()
     }
+}
+data class Student(
+    val fullName: String,
+    val course: Int
+) {
+    override fun toString(): String {
+        return fullName
+    }
+
 }
